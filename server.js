36fa4d8c5a1494206d218bcb71fdbd1c695a78d4
Vordock <saulodias.dev@gmail.com>
2024-10-23 15:12:30 -0300

    online settings on

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require ('express');
+const express = require('express');
 const http = require('http');
 const path = require('path');
 const { Server } = require('socket.io');
 const { randomUUID } = require('crypto');
 
-const APP = express;
+const APP = express();
 const HTTP_SERVER = http.createServer(APP);
 const SERVER = new Server(HTTP_SERVER);
 const PORT = 2108;
@@ -12,9 +12,8 @@ const PORT = 2108;
 APP.use(express.static(path.join(__dirname, '/public')));
 APP.use('/favicon.ico', (req, res) => res.status(204).end());
 
-APP.app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'games/1/index.html'))); // Ler o index da build dentro da pasta
+APP.get('/', (req, res) => res.sendFile(path.join(__dirname, 'games/1/index.html'))); // Ler o index da build dentro da pasta
 
-
-SERVER.listen(PORT, () => {
-    console.log('SERVER ONLINE IN:', PORT);
+HTTP_SERVER.listen(PORT, () => {
+    console.log('\nSERVER ONLINE IN:', `localhost:${PORT}`);
 });
