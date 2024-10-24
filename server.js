62817fa09eed20d0a1bc242d4375619902b46cd7
Vordock <saulodias.dev@gmail.com>
2024-10-24 09:57:27 -0300

    starting ws dev

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ APP.use('/favicon.ico', (req, res) => res.status(204).end());
 APP.get('/page', (req, res) => res.sendFile(path.join(__dirname, 'page/index.html')));
 APP.get('/games/1', (req, res) => res.sendFile(path.join(__dirname, 'games/1/index.html'))); // Ler o index da build dentro da pasta
 
+APP.get('/games/9', (req, res) => res.sendFile(path.join(__dirname, 'games/9/index.html'))); // Ler o index da build dentro da pasta
+
 HTTP_SERVER.listen(PORT, () => {
     console.log('\nSERVER ONLINE IN:', `localhost:${PORT}`);
+});
+
+SERVER.on(('connection'), (socket) => {
+
 });
