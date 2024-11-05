ef0c2035ed2b55cc957bbca7b843481ff28ec847
Vordock <saulodias.dev@gmail.com>
2024-11-05 17:37:07 -0300

    feat: first tests in cashout

--- public/page/script.js
+++ public/page/script.js
@@ -4,189 +4,190 @@ const SOCKET = io();
 const GAME_CONTAINER = document.getElementById("game-container");
 const RESTART_BTN = document.getElementById("restart-button");
 
-const WITHDRAW_BTN = document.getElementById("withdraw-button");
-const WITHDRAW_DISPLAY = document.getElementById("withdraw-display");
+const CASHOUT_BTN = document.getElementById("withdraw-button");
+const CASHOUT_DISPLAY = document.getElementById("withdraw-display");
 
 const BALANCE_DISPLAY = document.getElementById("balance-display");
 const FEEDBACK_DISPLAY = document.getElementById("feedback-display");
 
 const BET_INPUT = document.getElementById("bet-input");
-const BET_BUTTON = document.getElementById("bet-button");
+const BET_BTN = document.getElementById("bet-button");
 
 let userAuthenticated;
 let current_bet_value = 1;
-let current_bet_id = '';
+let current_bet_id = "";
 
-SOCKET.on('connect', () => {
+SOCKET.on("connect", () => {
+  SOCKET.emit("USER_AUTH", { gid: 777, opt: "", oid: "" }, (response) => {
+    console.log("AUTH RESPONSE: ", response);
 
-    SOCKET.emit('USER_AUTH', { gid: 777, opt: '', oid: '' }, (response) => {
+    userAuthenticated = response.status === 1;
 
-        console.log('AUTH RESPONSE: ', response);
+    if (response.status === 1) {
+      UpdateBalance(response.data.balance);
 
-        userAuthenticated = response.status === 1;
+      DrawBoard();
+    }
+  });
 
-        if (response.status === 1) {
-            UpdateBalance(response.data.balance);
-           
-            DrawBoard();
-        }
-    })
-
-    SOCKET.on('CRASH', () => {
-        Feedback('Game Over');
-        setTimeout(() => {
-            DrawBoard(); 
-          }, 2000)
-    });
-
+  SOCKET.on("CRASH", () => {
+    Feedback("Game Over");
+    setTimeout(() => {
+      DrawBoard();
+    }, 2000);
+  });
 });
 
 // Revelar a célula com base no resultado do servidor
 function OpenCell(index, result, cashout) {
+  const CELL = GAME_CONTAINER.children[index];
 
-    const CELL = GAME_CONTAINER.children[index];
-
-    if (result === 'crash') {
-        CELL.textContent = "💣";
-        CELL.classList.add("crash-cell"); // Adiciona classe de bomba
-    } else {
-        CELL.textContent = result;
-        CELL.classList.add("revealed-cell");
-        WITHDRAW_BTN.disabled = false;
-        UpdateCashout(cashout);
-    }
+  if (result === "crash") {
+    CELL.textContent = "💣";
+    CELL.classList.add("crash-cell"); // Adiciona classe de bomba
+  } else {
+    CELL.textContent = result;
+    CELL.classList.add("revealed-cell");
+    CASHOUT_BTN.disabled = false;
+    UpdateCashout(cashout);
+  }
 }
 
-// Atualizar pontuação
+// UPDATE BALANCE DISPLAY
 function UpdateBalance(newValue) {
-    BALANCE_DISPLAY.textContent = `Balance: ${newValue}`;
+  BALANCE_DISPLAY.textContent = `Balance: ${newValue}`;
 }
 
-// Atualizar pontuação
+// UPDATE CASHOUT DISPLAY
 function UpdateCashout(cashout) {
-    WITHDRAW_DISPLAY.textContent = `Cashout: ${cashout.toFixed(2)}`;
+  CASHOUT_DISPLAY.textContent = `Cashout: ${+cashout.toFixed(2)}`;
 }
 
 function DrawBoard() {
+  SOCKET.emit("START", (start_response) => {
+    current_bet_id = "";
 
-    SOCKET.emit('START', (start_response) => {
+    BET_INPUT.disabled = false;
 
-        current_bet_id = '';
+    GAME_CONTAINER.innerHTML = "";
+    GAME_CONTAINER.style.gridTemplateColumns = `repeat(${start_response.grid_size}, 60px)`;
 
-        BET_INPUT.disabled = false;
+    for (
+      let i = 0;
+      i < start_response.grid_size * start_response.grid_size;
+      i++
+    ) {
+      const CELL = document.createElement("div");
+      CELL.classList.add("cell", "blocked");
+      CELL.dataset.index = i;
 
-        GAME_CONTAINER.innerHTML = '';
-        GAME_CONTAINER.style.gridTemplateColumns = `repeat(${start_response.grid_size}, 60px)`;
-
-        for (let i = 0; i < start_response.grid_size * start_response.grid_size; i++) {
-            const CELL = document.createElement("div");
-            CELL.classList.add("cell", "blocked"); 
-            CELL.dataset.index = i;
-
-            CELL.addEventListener("click", () => {
-
-                if (!CELL.classList.contains("blocked")) {
-                    SOCKET.emit('PICK_CELL', i, (response) => {
-
-                        //console.log(response);
-                        if (response.status === 1) {
-                            OpenCell(response.index, response.result, response.cashout);
-                        } else {
-                            Feedback(`Server: ${response.message}`);
-                        }
-                    });
-                } else {
-                    Feedback(`Cell ${i + 1} is locked. Bet to play.`);
-                }
-
-            });
-
-            GAME_CONTAINER.appendChild(CELL);
+      CELL.addEventListener("click", () => {
+        if (!CELL.classList.contains("blocked")) {
+          SOCKET.emit("PICK_CELL", i, (response) => {
+            //console.log(response);
+            if (response.status === 1) {
+              OpenCell(response.index, response.result, response.cashout);
+            } else {
+              Feedback(`Server: ${response.message}`);
+            }
+          });
+        } else {
+          Feedback(`Cell ${i + 1} is locked. Bet to play.`);
         }
+      });
 
-        BET_INPUT.value = current_bet_value;
+      GAME_CONTAINER.appendChild(CELL);
+    }
 
-        Feedback('New game ready!');
-    });
+    BET_INPUT.value = current_bet_value;
+
+    Feedback("New game ready!");
+  });
 }
 
 function UnlockCells() {
-    document.querySelectorAll(".cell").forEach(cell => cell.classList.remove("blocked"));
+  document
+    .querySelectorAll(".cell")
+    .forEach((cell) => cell.classList.remove("blocked"));
 }
 
-WITHDRAW_BTN.addEventListener("click", () => {
-    SOCKET.emit('PLACE_CASHOUT', { current_bet_id: current_current_bet_id }, (response) => {
+CASHOUT_BTN.addEventListener("click", () => {
+  SOCKET.emit(
+    "PLACE_CASHOUT",
+    { current_bet_id: current_bet_id },
+    (response) => {
+      if (response.status === 1) {
+        Feedback("Cashout!");
 
-        if (response.status === 1) {
-            UpdateCashout(0); 
-            DrawBoard(); 
-        }
-    });
+        UpdateCashout(response.cashout_value);
+        UpdateBalance(response.balance);
+        setTimeout(() => {
+          DrawBoard();
+          UpdateCashout(0);
+        }, 2000);
+      }
+    }
+  );
 });
 
 BET_INPUT.addEventListener("blur", () => {
-    current_bet_value = parseFloat(BET_INPUT.value) || 0;
+  current_bet_value = parseFloat(BET_INPUT.value) || 0;
 });
 
 BET_INPUT.addEventListener("keydown", (event) => {
-    if (event.key === "Enter") {
-        BET_INPUT.blur();
-    }
+  if (event.key === "Enter") {
+    BET_INPUT.blur();
+  }
 });
 
-// Evento de aposta
-BET_BUTTON.addEventListener("click", () => {
+BET_BTN.addEventListener("click", () => {
+  if (current_bet_value > 0) {
+    SOCKET.emit("PLACE_BET", { bet_value: current_bet_value }, (response) => {
+      if (response.status === 1) {
+        UnlockCells();
+        BET_INPUT.disabled = true;
 
-    if (current_bet_value > 0) {
+        Feedback("Bet confirmed! Cells unlocked.");
 
-        SOCKET.emit('PLACE_BET', { bet_value: current_bet_value }, (response) => {
-            if (response.status === 1) {
-                UnlockCells();
-                BET_INPUT.disabled = true;
-
-                Feedback('Bet confirmed! Cells unlocked.');
-
-                UpdateBalance(response.balance);
-            }
-        });
-
-    } else {
-        Feedback('Insert a valid value to bet.');
-    }
+        UpdateBalance(response.balance);
+      }
+    });
+  } else {
+    Feedback("Insert a valid value to bet.");
+  }
 });
 
 RESTART_BTN.addEventListener("click", () => {
-    DrawBoard();
+  DrawBoard();
 });
 
 let diplayCheck = false;
 let feedback_list = [];
 
 function Feedback(text) {
-    feedback_list.push(text);
+  feedback_list.push(text);
 
-    const CLEAR = () => {
-        FEEDBACK_DISPLAY.textContent = '';
-        diplayCheck = false;
+  const CLEAR = () => {
+    FEEDBACK_DISPLAY.textContent = "";
+    diplayCheck = false;
 
-        if (feedback_list.length > 0) {
-            displayNextFeedback();
-        }
+    if (feedback_list.length > 0) {
+      displayNextFeedback();
     }
+  };
 
-    const displayNextFeedback = () => {
-        if (!diplayCheck && feedback_list.length > 0) {
+  const displayNextFeedback = () => {
+    if (!diplayCheck && feedback_list.length > 0) {
+      FEEDBACK_DISPLAY.textContent = feedback_list.shift(); // shift() to remove from start
+      diplayCheck = true;
 
-            FEEDBACK_DISPLAY.textContent = feedback_list.shift(); // shift() to remove from start
-            diplayCheck = true;
-
-            setTimeout(CLEAR, 1500);
-        }
+      setTimeout(CLEAR, 1500);
     }
+  };
 
-    if (!diplayCheck) {
-        displayNextFeedback();
-    }
+  if (!diplayCheck) {
+    displayNextFeedback();
+  }
 }
 
 // // Example usage
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
-const { Server } = require('socket.io');
-const { randomUUID } = require('crypto');
+const express = require("express");
+const http = require("http");
+const path = require("path");
+const { Server } = require("socket.io");
+const { randomUUID } = require("crypto");
 //const cors = require('cors');
 
 const APP = express();
@@ -20,32 +20,36 @@ const GRID_SIZE = 4;
 const BOMB_COUNT = 3;
 
 //APP.use(cors());
-APP.use(express.static(path.join(__dirname, '/public')));
-APP.use('/favicon.ico', (req, res) => res.status(204).end());
+APP.use(express.static(path.join(__dirname, "/public")));
+APP.use("/favicon.ico", (req, res) => res.status(204).end());
 
-APP.get('/page', (req, res) => res.sendFile(path.join(__dirname, 'page/index.html')));
-APP.get('/games/9', (req, res) => res.sendFile(path.join(__dirname, 'games/9/index.html')));
+APP.get("/page", (req, res) =>
+  res.sendFile(path.join(__dirname, "page/index.html"))
+);
+APP.get("/games/9", (req, res) =>
+  res.sendFile(path.join(__dirname, "games/9/index.html"))
+);
 
 class User {
-    constructor(name) {
-        this.name = name;
-        this.current_balance = '3521';
-        this.current_bet_id = '';
-        this.current_bet_value = 0;
-        this.current_cash_count = 0;
-        this.current_cashout = 0;
-    }
+  constructor(name) {
+    this.name = name;
+    this.current_balance = "3521";
+    this.current_bet_id = "";
+    this.current_bet_value = 0;
+    this.current_cash_count = 0;
+    this.current_cashout = 0;
+  }
 
-    ResetData() {
-        this.current_balance = '3521';
-        this.current_bet_id = '';
-        this.current_bet_value = 0;
-        this.current_cash_count = 0;
-        this.current_cashout = 0;
-    }
+  ResetData() {
+    this.current_balance = "3521";
+    this.current_bet_id = "";
+    this.current_bet_value = 0;
+    this.current_cash_count = 0;
+    this.current_cashout = 0;
+  }
 }
 
-let user = new User('SrBot');
+let user = new User("SrBot");
 let gameBoard = [];
 let gameOver = false;
 
@@ -54,208 +58,234 @@ let bet = 10;
 const BET_COEF = bet;
 
 const MULTIPLY_WEIGHT = [
-    { min: 1, max: 1, weight: 0.5 },
-    { min: 0.30, max: 0.41, weight: 5 },
-    { min: 0.20, max: 0.29, weight: 15 },
-    { min: 0.11, max: 0.19, weight: 35 },
-    { min: 0.05, max: 0.1, weight: 65 },
+  { min: 1, max: 1, weight: 0.5 },
+  { min: 0.3, max: 0.41, weight: 5 },
+  { min: 0.2, max: 0.29, weight: 15 },
+  { min: 0.11, max: 0.19, weight: 35 },
+  { min: 0.05, max: 0.1, weight: 65 },
 ];
 
 // Função para arredondar valores com precisão de duas casas decimais
 function roundToTwo(value) {
-    return Math.round(value * 100) / 100;
+  return Math.round(value * 100) / 100;
 }
 
 function WeightedRandomNumber(weightsArray) {
-    // Calcula o peso total acumulado
-    const maxWeight = Math.max(...weightsArray.map(elem => elem.weight));
+  // Calcula o peso total acumulado
+  const maxWeight = Math.max(...weightsArray.map((elem) => elem.weight));
 
-    let randomWeight = roundToTwo(Math.random() * maxWeight);
+  let randomWeight = roundToTwo(Math.random() * maxWeight);
 
-    for (let i = 0; i < weightsArray.length; i++) {
-        const element = weightsArray[i];
+  for (let i = 0; i < weightsArray.length; i++) {
+    const element = weightsArray[i];
 
-        if (randomWeight <= element.weight) {
-            const numberGet = roundToTwo(Math.random() * (element.max - element.min) + element.min);
+    if (randomWeight <= element.weight) {
+      const numberGet = roundToTwo(
+        Math.random() * (element.max - element.min) + element.min
+      );
 
-            console.log('\nChance:', `${roundToTwo(randomWeight)} / ${element.weight}`);
-            console.log('Multiply:', numberGet);
+      console.log(
+        "\nChance:",
+        `${roundToTwo(randomWeight)} / ${element.weight}`
+      );
+      console.log("Multiply:", numberGet);
 
-            return numberGet;
-        }
-    };
+      return numberGet;
+    }
+  }
 }
 
-IO_SERVER.on('connection', (_socket) => {
-    //console.log('\nPlayer Connected:', socket.id);
+IO_SERVER.on("connection", (_socket) => {
+  //console.log('\nPlayer Connected:', socket.id);
 
-    _socket.on('USER_AUTH', (data, callback) => {
+  _socket.on("USER_AUTH", (data, callback) => {
+    //console.log('USER AUTH:', data);
+    if (data.gid === 777) {
+      callback &&
+        callback({
+          status: 1,
+          data: { balance: user.current_balance },
+          message: "Player Authenticated!",
+        });
+    } else {
+      console.log("\nInvalid Game ID received...");
+    }
+  });
 
-        //console.log('USER AUTH:', data);
-        if (data.gid === 777) {
-            callback && callback({ status: 1, data: { balance: user.current_balance }, message: 'Player Authenticated!' })
-        } else {
-            console.log('\nInvalid Game ID received...');
-        }
-    });
+  _socket.on("START", (callback) => {
+    CreateGameBoard();
+    callback && callback({ grid_size: GRID_SIZE });
+  });
 
-    _socket.on('START', (callback) => {
+  // Evento de clique na célula
+  _socket.on("PICK_CELL", async (index, callback) => {
+    if (gameOver || gameBoard[index] === "open") return;
+
+    let result;
+
+    if (gameBoard[index] === "crash") {
+      result = "crash";
+      gameOver = true;
+
+      callback &&
+        callback({
+          status: 1,
+          index: index,
+          result: result,
+          cashout: parseFloat(user.current_cashout),
+        });
+    } else if (gameBoard[index] === "cash") {
+      // Gera o valor do multiplicador apenas uma vez
+      const CURRENT_GAIN = CalculateCellValue();
+      result = `+${CURRENT_GAIN}`;
+      callback &&
+        callback({
+          status: 1,
+          index: index,
+          result: result,
+          cashout: CalculateNewCashout(CURRENT_GAIN),
+        });
+    } else {
+      result = "empty";
+      callback &&
+        callback({
+          status: 0,
+          index: index,
+          result: result,
+          cashout: user.current_cashout,
+          message: "Unknown cell value.",
+        });
+    }
+
+    gameBoard[index] = "open";
+
+    // Checar se o jogador encontrou uma bomba
+    if (gameOver) {
+      IO_SERVER.emit("CRASH");
+    }
+  });
+
+  _socket.on("PLACE_BET", (data, callback) => {
+    console.log("Data content:", data);
+
+    let numericBalance = parseFloat(user.current_balance); //so pra garantir que ta lidando com numeros
+
+    const NEW_BET_ID = randomUUID();
+    user.current_bet_id = NEW_BET_ID;
+
+    console.log("\nBET GET:", data.bet_value);
+
+    if (numericBalance >= data.bet_value) {
+      numericBalance -= data.bet_value;
+
+      user.current_bet_value = data.bet_value;
+      user.current_balance = numericBalance.toFixed(2);
+      user.current_cashout = user.current_bet_value;
+
+      console.log("New Balance:", user.current_balance);
+
+      callback &&
+        callback({
+          status: 1,
+          bet_id: user.current_bet_id,
+          balance: user.current_balance,
+        });
+    } else {
+      console.log("\n  Not Enough Balance!:", user.current_balance);
+
+      callback &&
+        callback({ status: 0, message: "Player balance is not enough." });
+    }
+  });
+
+  // Evento de saque
+  _socket.on("PLACE_CASHOUT", (data, callback) => {
+    if (user.current_cash_count > 0 && data.bet_id === user.bet_id) {
+      callback &&
+        callback({
+          status: 1,
+          cashout_value: user.current_cashout,
+          balance: user.current_balance,
+        });
+      setTimeout(() => {
         CreateGameBoard();
-        callback && callback({ grid_size: GRID_SIZE });
-    });
+      }, 1000);
+    } else {
+      callback && callback({ status: 0, message: "Invalid Bet ID!" });
+    }
+  });
 
-    // Evento de clique na célula
-    _socket.on('PICK_CELL', async (index, callback) => {
-        if (gameOver || gameBoard[index] === 'open') return;
+  _socket.on("disconnect", () => {
+    console.log(`\nPlayer Disconnected: ${_socket.id}`);
 
-        let result;
-
-        if (gameBoard[index] === 'crash') {
-            result = 'crash';
-            gameOver = true;
-
-            callback && callback({ status: 1, index: index, result: result, cashout: user.current_cashout });
-
-        } else if (gameBoard[index] === 'cash') {
-            // Gera o valor do multiplicador apenas uma vez
-            result = `+${CalculateCellValue()}`;
-            callback && callback({ status: 1, index: index, result: result, cashout: user.current_cashout });
-
-        } else {
-            result = 'empty';
-            callback && callback({ status: 0, index: index, result: result, cashout: user.current_cashout, message: 'Unknown cell value.' });
-        }
-
-        gameBoard[index] = 'open';
-
-        // Checar se o jogador encontrou uma bomba
-        if (gameOver) {
-            IO_SERVER.emit('CRASH');
-        }
-    });
-
-    _socket.on('PLACE_BET', (data, callback) => {
-
-        console.log('Data content:', data);
-
-        let numericBalance = parseFloat(user.current_balance); //so pra garantir que ta lidando com numeros
-
-        const NEW_BET_ID = randomUUID();
-        user.current_bet_id = NEW_BET_ID;
-
-        console.log('\nAposta recebida:', data.bet_value);
-
-        if (numericBalance >= data.bet_value) {
-            numericBalance -= data.bet_value;
-
-            user.current_bet_value = data.bet_value;
-            user.current_balance = numericBalance.toFixed(2);
-            console.log('Novo saldo:', user.current_balance);
-
-            callback && callback({ status: 1, bet_id: user.current_bet_id, balance: user.current_balance });
-
-        } else {
-            console.log('\n  Not Enough Balance!:', user.current_balance);
-
-            callback && callback({ status: 0, message: 'Player balance is not enough.' });
-        }
-
-    });
-
-    // Evento de saque
-    _socket.on('PLACE_CASHOUT', (data, callback) => {
-        if (user.current_cash_count > 0 && data.bet_id === user.bet_id) {
-            callback && callback({ status: 1, cashout_value: user.current_cashout, balance: user.current_balance });
-            setTimeout(() => {
-                CreateGameBoard();
-            }, 1000);
-        } else {
-            callback && callback({ status: 0, message: 'Invalid Bet ID!' });
-        }
-
-    });
-
-    _socket.on('disconnect', () => {
-        console.log(`\nPlayer Disconnected: ${_socket.id}`);
-
-        user.ResetData();
-    });
-
+    user.ResetData();
+  });
 });
-
 
 // Função para criar um novo tabuleiro
 function CreateGameBoard() {
-    gameBoard = Array(GRID_SIZE * GRID_SIZE).fill(null);
+  gameBoard = Array(GRID_SIZE * GRID_SIZE).fill(null);
 
-    let starsPlaced = 0;
-    let bombsPlaced = 0;
-    let cellsFree = gameBoard.length;
+  let starsPlaced = 0;
+  let bombsPlaced = 0;
+  let cellsFree = gameBoard.length;
 
-    // Colocar bombas
-    while (bombsPlaced < BOMB_COUNT) {
-        const index = Math.floor(Math.random() * gameBoard.length);
-        if (gameBoard[index] === null) {
-            gameBoard[index] = 'crash';
-            bombsPlaced++;
-            cellsFree--;
-        }
+  // Colocar bombas
+  while (bombsPlaced < BOMB_COUNT) {
+    const index = Math.floor(Math.random() * gameBoard.length);
+    if (gameBoard[index] === null) {
+      gameBoard[index] = "crash";
+      bombsPlaced++;
+      cellsFree--;
     }
+  }
 
-    // Colocar estrelas
-    while (cellsFree > 0) {
-        const index = Math.floor(Math.random() * gameBoard.length);
-        if (gameBoard[index] === null) {
-            gameBoard[index] = 'cash';
-            starsPlaced++;
-            cellsFree--;
-        }
+  // Colocar estrelas
+  while (cellsFree > 0) {
+    const index = Math.floor(Math.random() * gameBoard.length);
+    if (gameBoard[index] === null) {
+      gameBoard[index] = "cash";
+      starsPlaced++;
+      cellsFree--;
     }
+  }
 
-    //console.log('gameBoard:', gameBoard);
-    gameOver = false;
-    console.log('\nNEW BOARD READY!');
+  //console.log('gameBoard:', gameBoard);
+  gameOver = false;
+  console.log("\nNEW BOARD READY!");
 }
 
 function CalculateBet(betValue) {
-
-    if (+user.current_balance >= betValue) {
-        user.current_bet_value = betValue;
-        user.current_cashout = betValue;
-    }
+  if (+user.current_balance >= betValue) {
+    user.current_bet_value = betValue;
+    user.current_cashout = betValue;
+  }
 }
 
-function CalculateCashout(cellValue) {
-    if (user.current_bet_value > 0) {
-        
-        user.current_cashout += roundToTwo(+cellValue);
-
-        const numBalance = parseFloat(user.current_balance);
-
-        return numBalance += user.current_cashout;
-    }
+function CalculateNewCashout(cellValue) {
+  if (user.current_bet_value > 0) {
+    return (user.current_cashout += roundToTwo(+cellValue));
+  }
 }
 
-function CalculateCellValue(){
+function CalculateCellValue() {
+  const multiplier = WeightedRandomNumber(MULTIPLY_WEIGHT);
 
-    const multiplier = WeightedRandomNumber(MULTIPLY_WEIGHT);
+  const GAIN = user.current_bet_value * multiplier;
 
-    const GAIN = user.current_bet_value * multiplier;
+  user.current_cashout += GAIN;
+  user.current_cash_count++;
 
-    user.current_cashout += GAIN;
-    user.current_cash_count++;
+  console.log("Multiply:", multiplier);
+  console.log("Gain:", roundToTwo(GAIN));
 
-    console.log('Multiplicador:', multiplier);
-    console.log('Ganho:', GAIN);
-
-    return multiplier;
+  return multiplier;
 }
-
 
 HTTP_SERVER.listen(PORT, () => {
-    console.log('\nSERVER ONLINE IN:', `localhost:${PORT}`);
-    // Inicializar o jogo no início
-    //CreateGameBoard();
+  console.log("\nSERVER ONLINE IN:", `localhost:${PORT}`);
+  // Inicializar o jogo no início
+  //CreateGameBoard();
 
-    //setInterval(() => (WeightedRandomNumber(MULTIPLY_WEIGHT)), 1000);
+  //setInterval(() => (WeightedRandomNumber(MULTIPLY_WEIGHT)), 1000);
 });
